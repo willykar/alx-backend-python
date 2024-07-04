2acb2847f2db350b300a3a170ee0e9fa6498a395
willykar <wilsonnjenga799@gmail.com>
2024-07-04 13:30:59 -0700

    4-define_variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""A module that defines and annotate variables with specified values
+a, an integer with a value of 1
+pi, a float with a value of 3.14
+i_understand_annotations, a boolean with a value of True
+school, a string with a value of “Holberton"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
