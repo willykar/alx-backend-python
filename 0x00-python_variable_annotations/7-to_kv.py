1107085de471d2e636b818f40934d15c8c4f99f6
willykar <wilsonnjenga799@gmail.com>
2024-07-04 14:04:40 -0700

    7-to_kv

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -3,8 +3,8 @@
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]
-    """ A type-annotated function to_kv that takes a
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """A type-annotated function to_kv that takes a
     string k and an int OR float v as arguments and
     returns a tuple"""
     return (k, v * v)
