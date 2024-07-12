ec622828d2cc61a75805c8cead22774a309aaba2
willykar <103494595+willykar@users.noreply.github.com>
2024-07-12 20:46:56 +0300

    Update 2-measure_runtime.py

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -14,3 +14,13 @@ module = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(module)
 
 wait_n = module.wait_n
+
+def measure_time(n: int, max_delay: int) -> float:
+    """Measure_time function that returns sorted lists containing
+       max_delay random values
+    """
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.time()
+    total_time = end_time - start_time
+    return total_time / n
